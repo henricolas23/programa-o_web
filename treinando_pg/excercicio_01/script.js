f1514301585ed7a04e7e40fc04885155443e5f4c
henricolas23 <henricolas23filizola@gmail.com>
2026-04-13 17:29:26 -0300

    add

---
+++ treinando_pg/excercicio_01/script.js
@@ -0,0 +1,22 @@
+let nome = "henrique";
+console.log(`seja bem vindo ${nome} aproveite o curso`);
+
+
+
+let numero1 = 10;
+
+if (numero1 > 0){
+    console.log("numero positivo");
+}
+ else if (numero1 < 0){
+    console.log("numero negativo");
+    }
+
+
+
+const nuemros = [1,2,3,4,5];
+console.log(nuemros[0]);
+console.log(nuemros[1]);
+console.log(nuemros[2]);
+console.log(nuemros[3]);
+console.log(nuemros[4]);
---
+++ treinando_pg/exercicio_02/script.js
@@ -0,0 +1,13 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < 10; i++) {
+    console.log(numeros[i]);
+}
+
+let numero1 = 20 
+let numero2 = 10
+let resultado = numero1 + numero2
+console.log(`o resultado é ${resultado}`);
+
+const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const existe = numero.includes(5);
+console.log(existe);
